b6825309fa436d79bf73769b78a97d3a09757003
paulpellom@gmail.com <paulpellom@gmail.com>
2018-02-14 10:50:03 -0500

    HW due

--- main.js
+++ main.js
@@ -1,27 +1,14 @@
-    $(document).ready(function(){
-        $("#header").hide(); $("#headline").hide();$("nav").hide();
-        $("#header").fadeIn(4000); $("#headline").fadeIn(5200);
-         $("nav").fadeIn(6000);
-       
-
-        
+    $(document).ready(function () {
+    $("#header").hide();
+    $("#headline").hide();
+    $("nav").hide();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    $("#header").fadeIn(4000);
+    $("#headline").fadeIn(5200);
+    $("nav").fadeIn(8000);
+   
 
 });
+   
+
