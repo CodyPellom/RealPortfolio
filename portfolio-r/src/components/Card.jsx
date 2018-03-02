eb2f1323510bd01df0c8ede8ea161d621ffdbf1d
paulpellom@gmail.com <paulpellom@gmail.com>
2018-03-02 18:14:44 -0500

    afternoon delight

---
+++ portfolio-r/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Styled from 'styled-components'
+
+class Card extends Component {
+    render() {
+        return (
+            <div>
+          
+            </div>
+        );
+    }
+}
+
+
+
+export default Card
---
+++ portfolio-r/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Styled from 'styled-components'
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+
+
+
+
+  
+export default Footer
